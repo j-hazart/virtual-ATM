5a1a4b7337797297accc52a8055d4f148247e78c
j-hazart <jessy.hazart@gmail.com>
2023-07-10 18:56:27 +0200

    add card number input

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,83 @@
+import { useState } from "react";
+import openEye from "../assets/eye.svg";
+import closeEye from "../assets/eye-slash.svg";
+import {
+  separateCardNumber,
+  hideCardNumber,
+} from "../services/formatCardNumbers";
+import "../styles/Home.css";
+
 export default function Home() {
-  return <p>Home page</p>;
+  const [isCardInserted, setIsCardInserted] = useState(false);
+  const [isEyeOpen, setIsEyeOpen] = useState(false);
+  const [inputCardNumbers, setInputCardNumbers] = useState("");
+  const [cardNumbers, setCardNumbers] = useState("#### #### #### ####");
+
+  /**
+   * The function "formatCardNumber" takes a card number value as input and formats it by separating it
+   * into groups of four digits and hiding all but the last two digits.
+   */
+  function formatCardNumber(value) {
+    let cardIdValue = "";
+    value.length === 0 && setCardNumbers("#### #### #### ####");
+    value.split("").forEach((number, i) => {
+      cardIdValue += separateCardNumber(i, 4) + hideCardNumber(i, number, 14);
+      setCardNumbers(cardIdValue);
+    });
+  }
+
+  /**
+   * The handleChange function updates the input number and formats the card number if the input value
+   * is a string of up to 16 digits.
+   */
+  function handleChange(e) {
+    const value = e.target.value;
+    if (/^\d{0,16}$/.test(value)) {
+      setInputCardNumbers(value);
+      formatCardNumber(value);
+    }
+  }
+
+  return (
+    <section id="home">
+      <section className="home-text">
+        <h1>Bienvenue</h1>
+        <p>
+          Pour commencer à utiliser le distributeur, entrez le numero de votre
+          carte et insérez la.
+        </p>
+        <div className="card-number-input">
+          <input
+            type="text"
+            name="cardId"
+            id="cardID"
+            onChange={(e) => handleChange(e)}
+            placeholder="1234567812345678"
+            value={inputCardNumbers}
+            autoComplete="off"
+            style={{ WebkitTextSecurity: `${isEyeOpen ? "none" : "disc"}` }}
+          />
+          <img
+            src={isEyeOpen ? openEye : closeEye}
+            alt="icon of an eye open or close"
+            onClick={() => setIsEyeOpen(!isEyeOpen)}
+          />
+        </div>
+      </section>
+      <section
+        className="home-card-container"
+        onClick={() => setIsCardInserted(!isCardInserted)}
+      >
+        <div className="card-hole">
+          <div className="holebar"></div>
+        </div>
+        <div className={`home-card ${isCardInserted ? "insert" : ""}`}>
+          <div className="puce"></div>
+          <p name="card-number" id="card-number">
+            {cardNumbers}
+          </p>
+        </div>
+      </section>
+    </section>
+  );
 }
---
+++ frontend/src/services/formatCardNumbers.js
@@ -0,0 +1,17 @@
+/**
+ * The function separates a card number by inserting a space every specified interval.
+ * @returns a space character (" ") if the index is greater than 0 and is divisible by the interval,
+ * otherwise it returns an empty string ("").
+ */
+export function separateCardNumber(index, interval) {
+  return index > 0 && index % interval === 0 ? " " : "";
+}
+
+/**
+ * The function hides a portion of a card number by replacing it with "#" characters.
+ * @returns "#" if the index is greater than or equal to the start value and less than the end value.
+ * Otherwise, it returns the original number.
+ */
+export function hideCardNumber(index, number, end, start = 0) {
+  return index >= start && index < end ? "#" : number;
+}
